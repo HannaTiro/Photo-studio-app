8161260e245fc061ff1aec87b55ee2e6f74d80ba
Hanna <hanna.tiro@edu.fit.ba>
2022-02-07 10:26:03 +0100

    filters on api

--- PhotoStudio/PhotoStudio/Filters/ErrorFilter.cs
+++ PhotoStudio/PhotoStudio/Filters/ErrorFilter.cs
@@ -10,22 +10,22 @@ namespace PhotoStudio.Filters
 {
     public class ErrorFilter:ExceptionFilterAttribute
     {
-        //public override void OnException(ExceptionContext context)
-        //{
-        //    if (context.Exception is UserException)
-        //    {
-        //        context.ModelState.AddModelError("ERROR", context.Exception.Message);                       
-        //        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-        //    }
-        //    else
-        //    {
-        //        context.ModelState.AddModelError("ERROR", "Greška na serveru");                             
-        //        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-        //    }
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.Exception is UserException)
+            {
+                context.ModelState.AddModelError("ERROR", context.Exception.Message);
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+            else
+            {
+                context.ModelState.AddModelError("ERROR", "Greška na serveru");
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
 
-        //    var list = context.ModelState.Where(x => x.Value.Errors.Count > 0).ToDictionary(x => x.Key, y => y.Value.Errors.Select(z => z.ErrorMessage));
+            var list = context.ModelState.Where(x => x.Value.Errors.Count > 0).ToDictionary(x => x.Key, y => y.Value.Errors.Select(z => z.ErrorMessage));
 
-        //    context.Result = new JsonResult(list);
-        //}
+            context.Result = new JsonResult(list);
+        }
     }
 }
