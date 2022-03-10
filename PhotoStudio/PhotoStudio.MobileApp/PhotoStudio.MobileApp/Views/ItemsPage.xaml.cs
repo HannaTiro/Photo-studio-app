920af4aa5b2d4e0a79ed5617fd70eddfbcceca2c
Hanna <hanna.tiro@edu.fit.ba>
2022-03-11 00:20:36 +0100

    Cleanup

--- PhotoStudio/PhotoStudio.MobileApp/PhotoStudio.MobileApp/Views/ItemsPage.xaml.cs
+++ PhotoStudio/PhotoStudio.MobileApp/PhotoStudio.MobileApp/Views/ItemsPage.xaml.cs
@@ -14,19 +14,10 @@ namespace PhotoStudio.MobileApp.Views
 {
     public partial class ItemsPage : ContentPage
     {
-        ItemsViewModel _viewModel;
-
         public ItemsPage()
         {
             InitializeComponent();
-
-            BindingContext = _viewModel = new ItemsViewModel();
         }
 
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-            _viewModel.OnAppearing();
-        }
     }
 }
